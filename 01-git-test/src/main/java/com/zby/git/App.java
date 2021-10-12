55b1fdf5f0e0b80ce4c3ff49cfe58761a53b6269
zhangsan <zhangsan@qq.com>
2021-10-12 21:03:43 +0800

    hot-fix conflict commit2

--- 01-git-test/src/main/java/com/zby/git/App.java
+++ 01-git-test/src/main/java/com/zby/git/App.java
@@ -13,5 +13,7 @@ public class App
         System.out.println( "third modify!" );
         System.out.println("hot-fix test");
         System.out.println("hot-fix 合并冲突");
+        System.out.println("-----------");
+        System.out.println("hot-fix 冲突合并");
     }
 }
