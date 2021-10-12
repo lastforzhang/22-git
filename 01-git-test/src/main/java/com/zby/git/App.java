e8acc44a638af90c4ef12d604efadd29d5a2ce34
zhangsan <zhangsan@qq.com>
2021-10-12 20:52:24 +0800

    hot-fix commit1

--- 01-git-test/src/main/java/com/zby/git/App.java
+++ 01-git-test/src/main/java/com/zby/git/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "second modify!" );
         System.out.println( "third modify!" );
+        System.out.println("hot-fix test");
     }
 }
